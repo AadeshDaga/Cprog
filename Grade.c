0cc7e884d1c44de8a5b83a6097753b141e11bea3
AadeshDaga <70771548+AadeshDaga@users.noreply.github.com>
2020-09-21 09:46:11 +0530

    Grading system

---
+++ Grade.c
@@ -0,0 +1,29 @@
+#include<stdio.h> 
+int main() 
+{
+ float marks;
+ char grade;
+printf("enter marks: ");
+scanf("%f", &marks);
+if (marks >=90)
+{
+grade = 'A';
+}
+else if (marks>=80 &&marks<90) 
+{
+grade = 'B';
+}
+else if (marks>=70 &&marks<80) 
+{
+grade = 'C';
+}
+else if (marks>=70 &&marks<80) 
+{
+grade = 'D';
+}
+else {
+grade = 'E';
+}
+ printf("Your grade is %c", grade);
+return 0;
+ }
